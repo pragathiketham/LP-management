dd2d7adf01ca5deb7445c03e9b790cd97cf85ada
pragathiketham <55224223+pragathiketham@users.noreply.github.com>
2019-10-14 07:23:09 +0530

    Add files via upload

---
+++ LPBatchManagentSystem/src/com/virtusa/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.virtusa.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRepository {
+
+	
+	private Map<String,String> userRepository=null;
+	public UserRepository() {
+		userRepository=new HashMap<String ,String>();
+	}
+	
+	public void 
+}
